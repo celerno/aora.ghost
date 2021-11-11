193dcc91e0fc5035c9e83794aeb170251b57c7f7
Celerino Herrera <celerino.herrera@valtech.com>
2021-11-11 08:32:05 -0700

    contact- forms

--- assets/js/lib/cotizar.js
+++ assets/js/lib/cotizar.js
@@ -1,7 +1,4 @@
-var formCotiza = $('#cotiza-form'),
-    url = 'https://script.google.com/macros/s/AKfycbwkcxG4TDoT0dwGrk7bm2YJRBgZvqHonn7tLkYV-BXtc2Og3i7HYGm9RIOcyCv0suh3cg/exec'
-
-$(formCotiza).on('submit', function(e) {
+let entrada = function(e) {
     e.preventDefault();
     var jqxhr = $.ajax({
         url: url,
@@ -9,14 +6,29 @@ $(formCotiza).on('submit', function(e) {
         dataType: "json",
         data: $(formCotiza).serializeObject(),
         success: function() {
-            let btn = document.querySelector("#cotiza-form > div > button");
-            btn.classList.toggle("is_active");
-            if (window.location.href.includes('cotizar')) {
-                window.location.href = "/gracias";
-            } else {
+            try {
+                let btn = document.querySelector("#cotiza-form > div > button");
+                btn.classList.toggle("is_active");
+            } catch (error) {
+
+            }
+
+            if (window.location.href.includes('request-quote') || window.location.href.includes('/en')) {
                 window.location.href = "/thank-you";
+            } else {
+                window.location.href = "/gracias";
             }
         }
 
     });
-});
+}
+
+let formCotiza = $('#cotiza-form'),
+    url = 'https://script.google.com/macros/s/AKfycbwkcxG4TDoT0dwGrk7bm2YJRBgZvqHonn7tLkYV-BXtc2Og3i7HYGm9RIOcyCv0suh3cg/exec'
+let formContact = $('#contact-form');
+if (formContact !== undefined) {
+    $(formContact).on('submit', entrada);
+}
+if (formCotiza !== undefined) {
+    $(formCotiza).on('submit', entrada);
+}
